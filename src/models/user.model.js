88760630e68bb84a73c4b750472a748f69121af4
ashwaniverma01 <ashwaniverma9696@gmail.com>
2025-04-15 09:29:38 +0530

    models

---
+++ src/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+const userSchema=new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+        },
+        fullName:{
+            type:String,
+            required:true,
+            index:true
+        },
+        avatar:{
+            type:String,
+            required:true
+        },
+        coverImage:{
+            type:String,
+           
+        },
+        watchHistory:[
+            {
+                type:String,
+                required:true
+            }
+        ],
+        refreshToken:{
+            type:String
+        }
+
+
+},
+{timestamps:true})
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods.isPasswordCorrect=async function (password) {
+    return await bcrypt.compare(password,this.password)
+    
+}
+export const User=mongoose.model("User",userSchema)

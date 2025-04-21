37d6e05351de5b499dada98129f96d0fdaf970ec
ashwaniverma01 <ashwaniverma9696@gmail.com>
2025-04-22 03:17:04 +0530

    verify jwt and remove accessToken and refreshToken from browser and also remove refreshToken from database attemp to logout the user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -149,4 +149,33 @@ const loginUser = asyncHandler(async (req, res)=>{
 
 })
 
-export { registerUser, loginUser};
+const logoutUser = asyncHandler(async (req, res)=>{
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken:undefined
+      }
+    },
+    {
+      new:true
+    }
+
+  )
+  const options={
+    httpOnly:true,
+    secure:true
+  }
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(new ApiResponse(200, {}, "User logged out"))
+
+})
+
+export { 
+  registerUser, 
+  loginUser,
+  logoutUser
+};
